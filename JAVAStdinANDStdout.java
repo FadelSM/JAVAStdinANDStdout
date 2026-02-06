85beb5a78e6e2ed93bfbfcc4a910cb06d5108ebb
FadelSM <rasyahstore8888@gmail.com>
2026-02-06 19:44:28 +0700

    Add files via upload

---
+++ JAVAStdinANDStdout.java
@@ -0,0 +1,20 @@
+import java.util.;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+         Complete this line
+         Complete this line
+
+        System.out.println(42);
+        System.out.println(100);
+        System.out.println(125);
+        scan.close();
+         Complete this line
+         Complete this line
+    }
+}
